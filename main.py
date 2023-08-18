06c734d13a2b7585f755f15a7ac52a672e6b74ea
ata <your.email@example.com>
2023-08-18 19:29:33 +0330

    temp commit .

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import time
 import json
+
+import networkx as nx
+
 from graph import Graph
 from hub_lpa import HubLpa
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
+from networkx.generators.community import LFR_benchmark_graph
+from collections import Counter
 
 def get_real_results(path):
     with open(path, 'r') as file:
@@ -28,7 +33,6 @@ def load_datasets():
 if __name__ == "__main__":
     datasets = load_datasets().items()
     for dataset, truth in datasets:
-
         graph = Graph(dataset)
         hup_lpa = HubLpa(graph=graph)
         algorithm_start_time = time.time()
@@ -46,3 +50,43 @@ if __name__ == "__main__":
                 get_real_results(truth)
             ))
         print('------------------------------------------------------' + '\n')
+
+    # lfr
+
+    # graph = Graph(LFR_benchmark_graph(
+    #     5000, min_degree=20, max_degree=50, min_community=20, max_community=100, tau2=2, tau1=2, mu=0.7
+    # ))
+    # nodes = graph.get_nodes_and_data()
+    # finded_communities = dict()
+    # community_counter = 0
+    # real_results = []
+    #
+    # for node in nodes:
+    #     community = node[1]['community']
+    #     if community not in finded_communities.values():
+    #         finded_communities[community_counter] = community
+    #         community_counter += 1
+    #         real_results.append(community_counter)
+    #     else:
+    #         for finded_community in finded_communities.items():
+    #             if finded_community[1] == community:
+    #                 real_results.append(finded_community[0])
+    #
+    # hup_lpa = HubLpa(graph=graph)
+    # algorithm_start_time = time.time()
+    # print('alghorithm start')
+    # iteration_count = hup_lpa.perform_algorithm()
+    # algorithm_finish_time = time.time()
+    # print('data set   : ', 'lfr 500')
+    # print('algorithm executed in  :', algorithm_finish_time - algorithm_start_time)
+    # print('algorithm iteration count : ', iteration_count)
+    # print('communities found  : ', len(graph.get_labels()))
+    # print('gained modularity  :', graph.calculate_modularity())
+    # if True:
+    #     print('gained nmi  : ', normalized_mutual_info_score(
+    #         graph.get_graph_labels(),
+    #         real_results
+    #     ))
+    # print('------------------------------------------------------' + '\n')
+
+
