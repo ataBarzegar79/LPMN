198e2dd42aa7b5eaef816eb35603346524353f91
ata <your.email@example.com>
2023-08-24 18:43:22 +0330

    optimize code .

--- hub_lpa.py
+++ hub_lpa.py
@@ -9,33 +9,41 @@ from label_update import LabelUpdate
 class HubLpa:
     def __init__(self, graph: Graph):
         self.graph = graph
+        self.nodes = self.graph.get_nodes()
 
     def perform_algorithm(self):
-        nodes = self.graph.get_nodes()
-        for node in nodes:  # label initialization  => O(n)
+        nodes_additional_data = dict()
+        for node in self.nodes:
+            node_degree = self.graph.get_node_degree(node=node)
+            node_neighbours = self.graph.get_node_neighbours(node=node)
+            nodes_additional_data[node] = {
+                'degree': node_degree,
+                'neighbours': node_neighbours
+            }
             self.graph.set_label_to_node(label=node, node=node)
-        nodes_based_on_importance = self.get_important_nodes(nodes=nodes)  # => O(nk)
+        nodes_based_on_importance = self.get_important_nodes(nodes_additional_data)
         clustering = nx.clustering(self.graph.graph)
         page_rank = nx.pagerank(self.graph.graph)
         counter = 0
         while True:
-            LABELS_BEFORE = [self.graph.get_node_current_label(node) for node in nodes]
+            LABELS_BEFORE = [self.graph.get_node_current_label(node) for node in self.nodes]
             counter += 1
             for node in nodes_based_on_importance:
-                new_label = self.get_new_label_of_node(node[0], clustering, page_rank)
+                new_label = self.get_new_label_of_node(node[0], clustering, page_rank, nodes_additional_data)
                 self.graph.set_label_to_node(new_label, node[0])
-            LABELS_AFTER = [self.graph.get_node_current_label(node) for node in nodes]
+            LABELS_AFTER = [self.graph.get_node_current_label(node) for node in self.nodes]
             if LABELS_BEFORE == LABELS_AFTER:
                 break
             if counter > 10:
                 break
-        merge_phase.merge_algorithm(self.graph)
+
+        # merge_phase.merge_algorithm(self.graph)
         return counter
 
-    def get_important_nodes(self, nodes):
+    def get_important_nodes(self, nodes_additional_data: dict):
         priority = Priority()
-        return priority.give_nodes_priority(self.graph)
+        return priority.give_nodes_priority(self.nodes, nodes_additional_data)
 
-    def get_new_label_of_node(self, node: int, clustering: dict, page_rank: dict) -> int:
+    def get_new_label_of_node(self, node: int, clustering: dict, page_rank: dict, nodes_additional_data: dict) -> int:
         label_update = LabelUpdate()
-        return label_update.retrieve_new_label(node, self.graph, clustering, page_rank)
+        return label_update.retrieve_new_label(node, self.graph, clustering, page_rank, nodes_additional_data)
--- label_update.py
+++ label_update.py
@@ -2,50 +2,25 @@ import networkx as nx
 
 
 class LabelUpdate:
-    def retrieve_new_label(self, node, graph, clustering: dict, page_rank: dict):
-        return self.method_two(graph, node,clustering,page_rank)
-        # return self.method_one(graph, node)
-
-    def method_one(self, graph, node):
-        node_neighbors = graph.get_node_neighbours(node=node)
-        label_count = dict()
-        for neighbor in node_neighbors:
-            label = graph.get_node_current_label(node=neighbor)
-            if label not in label_count:
-                label_count[label] = 1
-            else:
-                label_count[label] += 1
-        max_value = max(label_count.values())
-        labels_with_max_value = [label for label, count in label_count.items() if count == max_value]
-        if len(labels_with_max_value) > 1:
-            return self.method_two(graph, node)
-        else:
-            return labels_with_max_value[0]
-
-    def method_two(self, graph, node, clustering: dict, page_rank: dict) -> int:
-        node_neighbors = graph.get_node_neighbours(node=node)
+    def retrieve_new_label(self, node, graph, clustering: dict, page_rank: dict, additional_data: dict):
+        node_neighbors = additional_data[node]['neighbours']
         neighbors_score = dict()
         for neighbor in node_neighbors:
-            neighbor_neighbors = set(graph.get_node_neighbours(neighbor))
+            neighbor_neighbors = set(additional_data[neighbor]['neighbours'])
             mutual_neighbors = neighbor_neighbors & set(node_neighbors)
             neighbors_score[neighbor] = len(mutual_neighbors)
-        max_value = max(neighbors_score.values())
-        neighbors_with_maximum_score = [key for key, value in neighbors_score.items() if value == max_value]
+        neighbors_with_maximum_score = self.find_dictionary_maximum_key(dictionary=neighbors_score)
         if len(neighbors_with_maximum_score) > 1:
             if self.check_maximum_values_are_zero(neighbors_with_maximum_score, neighbors_score):
                 new_scores = dict()
-                # sum = 0
                 for neighbor in neighbors_with_maximum_score:
-                    # for second_neighbor in graph.get_node_neighbours(neighbor):
-                    #     sum += clustering[second_neighbor]
                     new_scores[neighbor] = page_rank[neighbor] ** (clustering[neighbor] + 1)
-                max_score = max(new_scores.values())
-                neighbors_with_maximum_score = [key for key, value in new_scores.items() if value == max_score]
-                return graph.get_node_current_label(neighbors_with_maximum_score[0])
+                neighbors_with_maximum_new_score = self.find_dictionary_maximum_key(new_scores)
+                return graph.get_node_current_label(neighbors_with_maximum_new_score[0])
             else:
                 max_degree_neighbor = (None, 0)
                 for neighbor in neighbors_with_maximum_score:
-                    neighbor_degree = graph.get_node_degree(neighbor)
+                    neighbor_degree = additional_data[neighbor]['degree']
                     if neighbor_degree > max_degree_neighbor[1]:
                         max_degree_neighbor = (neighbor, neighbor_degree)
                 return graph.get_node_current_label(max_degree_neighbor[0])
@@ -53,7 +28,11 @@ class LabelUpdate:
             return graph.get_node_current_label(neighbors_with_maximum_score[0])
 
     def check_maximum_values_are_zero(self, neighbors_with_maximum_score: list, neighbors_score: dict) -> bool:
-        for key in neighbors_with_maximum_score:
-            if neighbors_score[key] != 0:
+        for neighbor in neighbors_with_maximum_score:
+            if neighbors_score[neighbor] != 0:
                 return False
         return True
+
+    def find_dictionary_maximum_key(self, dictionary: dict) -> list:
+        max_value = max(dictionary.values())
+        return [key for key, value in dictionary.items() if value == max_value]
