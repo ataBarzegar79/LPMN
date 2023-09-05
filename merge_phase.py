0363aa6f2b336e2f259b2a21aaf99ee883d9800a
Ehsan Aghayari <ehsanaghayari313@gmail.com>
2023-09-05 23:24:03 +0330

    merge phase minor changes

--- merge_phase.py
+++ merge_phase.py
@@ -3,55 +3,34 @@ import time
 from graph import Graph
 
 
-def merge_algorithm(graph: Graph, nodes_additional_data):
+def merge_algorithm(graph: Graph, nodes_additional_data, page_rank):
     for i in range(2):
-        iter_start_merge = time.time()
-
-        communities_start = time.time()
-        communities = get_communities(nodes_additional_data, nodes_additional_data, graph)
+        communities = get_communities(nodes_additional_data, nodes_additional_data, graph, page_rank)
 
         small_communities = get_small_communities(nodes_additional_data, communities)
         large_communities = get_large_communities(nodes_additional_data, communities)
-        target_node_of_large_communities = large_community_target_nodes(large_communities)
-        communities_end = time.time()
 
-        # print('communities found in : ', communities_end - communities_start)
+        target_node_of_large_communities = large_community_target_nodes(large_communities)
 
         for small_community_label in small_communities:
             small_community = small_communities[small_community_label]
             target_node = small_community[1]
 
-            inner_edge_start = time.time()
             inner_edge_and_large_neighbors = get_small_community_inner_edge(small_community, small_community_label,
                                                                             nodes_additional_data, large_communities,
                                                                             graph)
-            inner_edge_end = time.time()
-            # print('inner_edge finished in : ', inner_edge_end - inner_edge_start)
 
-            dcn_started = time.time()
             large_community_label = return_largest_dcn(target_node, target_node_of_large_communities,
                                                        inner_edge_and_large_neighbors,
                                                        nodes_additional_data, graph)
-            dcn_ended = time.time()
 
-            # print('dcn calculated in : ', dcn_ended - dcn_started)
-
-            merge_list_started = time.time()
             need_to_update_list = merging_list(small_community[0], large_community_label,
                                                inner_edge_and_large_neighbors['inner_edge'],
                                                nodes_additional_data, graph)
-            merge_list_ended = time.time()
-
-            # print('merge list founded in : ', merge_list_ended - merge_list_started)
 
             if need_to_update_list:
-                merge_operation_started = time.time()
+                # print(need_to_update_list)
                 merging_operation(need_to_update_list, graph)
-                merge_operation_ended = time.time()
-
-                # print('merge operation finished in : ', merge_operation_ended - merge_operation_started)
-        iter_end_merge = time.time()
-        # print('merging iteration finished in : ', iter_end_merge - iter_start_merge)
 
 
 def calculate_dcn(node_v, node_i, nodes_additional_data, graph: Graph):  # this is really ok
@@ -136,7 +115,7 @@ def large_community_target_nodes(large_communities):
     return large_community_containing_target_nodes
 
 
-def get_communities(nodes, nodes_additional_data, graph: Graph):
+def get_communities(nodes, nodes_additional_data, graph: Graph, page_rank):
     communities = dict()
     for node in nodes:  # => O(n)
         node_current_label = graph.get_node_current_label(node)
@@ -145,12 +124,12 @@ def get_communities(nodes, nodes_additional_data, graph: Graph):
         else:
             communities[node_current_label][0].append(node)
     for community in communities:
-        candidate_for_target_node_degree = 0
+        candidate_for_target_node_page_rank = 0
         for node in communities[community][0]:
-            node_degree = nodes_additional_data[node]['degree']
-            if node_degree >= candidate_for_target_node_degree:
+            p_rank = page_rank[node]
+            if p_rank >= candidate_for_target_node_page_rank:
                 target_node = node
-                candidate_for_target_node_degree = node_degree
+                candidate_for_target_node_page_rank = p_rank
         communities[community].append(target_node)
     return communities
 
